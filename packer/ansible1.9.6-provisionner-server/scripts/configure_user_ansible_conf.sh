84754794f5b55bc5fe7b315a0f6c9f4847148fcf
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-02 14:55:14 +0200

    up dockerKlean and ansible.base close target

--- packer/ansible1.9.6-provisionner-server/scripts/configure_user_ansible_conf.sh
+++ packer/ansible1.9.6-provisionner-server/scripts/configure_user_ansible_conf.sh
@@ -8,7 +8,7 @@ mkdir ~/.aws
 echo "[default]\nregion = eu-west-1" > ~/.aws/config
 echo "[default]\naws_secret_access_key = ${ANSIBLE_SAK}\naws_access_key_id = ${ANSIBLE_AKI}" > ~/.aws/credentials
 
-git clone $ANSIBLE_REPO ~/ANSIBLE
+git clone $ANSIBLE_REPO ~/ansible.base
 mv /tmp/files/ansible.ec2.ini /home/admin/.ansible.ec2.ini
 
 echo "\nexport EC2_INI_PATH=~/.ansible.ec2.ini" >> ~/.bashrc
