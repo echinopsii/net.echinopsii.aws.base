437fcc15383e13c0f20f4824f0784b54cf1e7176
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-16 13:56:22 +0200

    [ACC-485] add new ansible provisionner packer ami builder

---
+++ packer/ansible-last-provisionner-server/scripts/install_ansible_debian.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash 
+set -e
+set -x
+
+_BDEP="python-pip libffi-dev libpython-dev libssl-dev git libyaml-dev"
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Installing dependencies..."
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y ${_BDEP}
+
+pip install setuptools==11.3
+pip install ansible
