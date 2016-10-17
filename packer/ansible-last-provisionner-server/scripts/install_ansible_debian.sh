9180fa69aba970d8137807d51660ef2993a664d5
Mathilde Ffrench <mathilde.ffrench@echinopsii.net>
2016-10-17 09:00:45 +0200

    [ACC-485] add tooling on ansible provisionner

--- packer/ansible-last-provisionner-server/scripts/install_ansible_debian.sh
+++ packer/ansible-last-provisionner-server/scripts/install_ansible_debian.sh
@@ -2,13 +2,13 @@
 set -e
 set -x
 
-_BDEP="python-pip libffi-dev libpython-dev libssl-dev git libyaml-dev"
+DEP="python-pip libffi-dev libpython-dev libssl-dev git libyaml-dev vim net-tools netcat"
 export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing dependencies..."
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y ${_BDEP}
+apt-get install -y ${DEP}
 
 pip install setuptools==11.3
 pip install ansible
